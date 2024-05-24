aaab409c21d5bd9dd3a2702377b4a42e466699d5
Ferreira <guilherme.ferreira@paynet.net.br>
2024-05-24 20:08:56 -0300

    Correção import de entidade

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from 'src/models/user.model';
+import { User } from 'src/models/entitites/user.entity';
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 
